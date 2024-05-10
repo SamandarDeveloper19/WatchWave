77866d659a8314bc0d97b23d4da453e6978f450a
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-05-10 21:45:46 +0500

    DATA: Add VideoMetadata

---
+++ WatchWave.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,32 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.SqlServer;
+
+namespace WatchWave.Api.Brokers.Storages
+{
+    public class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose()
+        { }
+    }
+}
---
+++ WatchWave.Api/Models/VideoMetadatas/VideoMetadata.cs
@@ -0,0 +1,18 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+namespace WatchWave.Api.Models.VideoMetadatas
+{
+    public class VideoMetadata
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BlobPath { get; set; }
+        public string Thubnail { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
