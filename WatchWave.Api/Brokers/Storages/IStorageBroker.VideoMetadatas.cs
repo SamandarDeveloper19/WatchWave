8e7b5a6898234937234ec50115a329a1801c3f5a
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-28 17:11:17 +0500

    CONTROLLERS: Add Video Metadata Post Method

--- WatchWave.Api/Brokers/Storages/IStorageBroker.VideoMetadatas.cs
+++ WatchWave.Api/Brokers/Storages/IStorageBroker.VideoMetadatas.cs
@@ -3,17 +3,16 @@
 // Free To Use To Find Comfort and Peace
 //==================================================
 
-using System.Runtime.CompilerServices;
 using WatchWave.Api.Models.VideoMetadatas;
 
 namespace WatchWave.Api.Brokers.Storages
 {
-    public partial interface IStorageBroker
-    {
-        ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata);
-        IQueryable<VideoMetadata> SelectAllVideoMetadatas();
-        ValueTask<VideoMetadata> SelectVideoMetadataByIdAsync(Guid videoMetadataId);
-        ValueTask<VideoMetadata> UpdateVideoMetadataAsync(VideoMetadata videoMetadata);
-        ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata);
-    }
+	public partial interface IStorageBroker
+	{
+		ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata);
+		IQueryable<VideoMetadata> SelectAllVideoMetadatas();
+		ValueTask<VideoMetadata> SelectVideoMetadataByIdAsync(Guid videoMetadataId);
+		ValueTask<VideoMetadata> UpdateVideoMetadataAsync(VideoMetadata videoMetadata);
+		ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata);
+	}
 }
--- WatchWave.Api/Controllers/HomeController.cs
+++ WatchWave.Api/Controllers/HomeController.cs
@@ -3,33 +3,17 @@
 // Free To Use To Find Comfort and Peace
 //==================================================
 
-using FluentAssertions.Specialized;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
-using WatchWave.Api.Models.VideoMetadatas;
-using WatchWave.Api.Services.VideoMetadatas;
 
 namespace WatchWave.Api.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class HomeController : RESTFulController
-    {
-        private readonly IVideoMetadataService VideoMetadataService;
-
-		public HomeController(IVideoMetadataService videoMetadataService)
-		{
-			VideoMetadataService = videoMetadataService;
-		}
-
-		/*[HttpGet]
-        public ActionResult<string> Get() =>
-            Ok("Hello Mario, the princes is in another castle");*/
-
-        [HttpPost]
-        public async ValueTask<ActionResult<VideoMetadata>> GetVideoMetadata(VideoMetadata videoMetadata) =>
-            await this.VideoMetadataService.AddVideoMetadataAsync(videoMetadata);
-
-
-    }
+	[ApiController]
+	[Route("api/[controller]")]
+	public class HomeController : RESTFulController
+	{
+		[HttpGet]
+		public ActionResult<string> Get() =>
+			Ok("Hello Mario, the princes is in another castle");
+	}
 }
